ccd00be153edb44beb0015734b93f31db8a961f5
Júlio César <julczar21@outlook.com>
2019-11-13 21:30:26 -0300

    Ajustes leves

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
 let modalAtivo = false
+const modal = document.getElementById("modal")
 
-function criaModal(item) {
+function criaModal(i) {
     if (!modalAtivo) {
         modalAtivo = true
         modal.style.display = "block"
-        let foto = item.src
-        let nome = item.name
-        let descricao = item.parentElement
+        let foto = document.querySelectorAll(".equipe")[i].src
+        let nome = document.querySelectorAll(".equipe")[i].name
+        let descricao = document.querySelectorAll(".equipe")[i].parentElement
         descricao = descricao.textContent
         modal.insertAdjacentHTML('beforeend', `
-        <div class="content">
-            <div id="fechar"><a onclick='fecharModal(this)' href='#'><i class="material-icons">clear</i></a></div>
+        <div id="modalJS" class="content">
+            <div id="fechar"><a onclick='fecharModal()' href='#'><i class="material-icons">clear</i></a></div>
             <img id="foto" src="${foto}" alt="Foto do membro clicado">
             <div id="nome">${nome}</div>
             <div id="descricao">${descricao}</div>
@@ -19,8 +20,8 @@ function criaModal(item) {
     }
 }
 
-function fecharModal(item){
+function fecharModal() {
     modalAtivo = false
     modal.style.display = "none"
-    item.parentElement.parentElement.remove()
+    document.getElementById("modalJS").remove()
 }
